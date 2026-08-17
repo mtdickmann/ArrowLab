594f2cd0a3ebff6b0123637a8ede7c9805c4f86d
mtdickmann <mtdickmann@web.de>
2026-08-17 20:33:41 +0200

    Bump firmware version to 0.3.5

--- include/Version.h
+++ include/Version.h
@@ -8,7 +8,7 @@ namespace Version
 
   constexpr uint8_t MAJOR = 0;
   constexpr uint8_t MINOR = 3;
-  constexpr uint8_t PATCH = 4;
+  constexpr uint8_t PATCH = 5;
 
   constexpr char STATUS[] = "DEV";
 }
