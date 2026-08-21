a6f11990deb5f529642b2c0fa57f5a79ed9a7c34
mtdickmann <mtdickmann@web.de>
2026-08-21 20:47:55 +0200

    Bump ArrowLab firmware to v0.3.11

--- include/Version.h
+++ include/Version.h
@@ -8,7 +8,7 @@ namespace Version
 
   constexpr uint8_t MAJOR = 0;
   constexpr uint8_t MINOR = 3;
-  constexpr uint8_t PATCH = 10;
+  constexpr uint8_t PATCH = 11;
 
   constexpr char STATUS[] = "DEV";
 }
